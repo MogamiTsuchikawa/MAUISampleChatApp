5147d73ae2437f6b78f538a28c9e0304973fc0b1
mogami <atsuyatsuchikawa@gmail.com>
2022-03-27 03:32:01 +0900

    init

---
+++ MAUISampleChatApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MAUISampleChatApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
